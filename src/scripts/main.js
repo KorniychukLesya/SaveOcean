0ad11bd22062f2ebe7d1db6f371a4576f04f8c0d
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-11-30 22:49:34 +0200

    slider implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,51 +1,94 @@
-'use strict';
+"use strict";
+let menuBtn = document.querySelector(".header__menu-btn");
+let menuLinkAboutUs = document.querySelector(".menu__link--aboutUs");
+let menuLinkOurProjects = document.querySelector(".menu__link--ourProjects");
+let menuLinkStories = document.querySelector(".menu__link--stories");
+let menuLinkJoinTeam = document.querySelector(".menu__link--joinTeam");
+let menuLinkContactUs = document.querySelector(".menu__link--contactUs");
+let menuLinkSocial = document.querySelector(".menu__social");
 
-let menuBtn = document.querySelector('.header__menu-btn');
-let menuLinkAboutUs = document.querySelector('.menu__link--aboutUs');
-let menuLinkOurProjects = document.querySelector('.menu__link--ourProjects');
-let menuLinkStories = document.querySelector('.menu__link--stories');
-let menuLinkJoinTeam = document.querySelector('.menu__link--joinTeam');
-let menuLinkContactUs = document.querySelector('.menu__link--contactUs');
-let menuLinkSocial = document.querySelector('.menu__social');
-
-let menu = document.querySelector('.menu');
-let main = document.querySelector('.main');
-let header__logo = document.querySelector('.header__logo');
-let header__menu = document.querySelector('.header__menu');
-let footer = document.querySelector('.footer');
+let menu = document.querySelector(".menu");
+let main = document.querySelector(".main");
+let header__logo = document.querySelector(".header__logo");
+let header__menu = document.querySelector(".header__menu");
+let footer = document.querySelector(".footer");
 
 const classToggle = function() {
-	menuBtn.classList.toggle('active');
-  menu.classList.toggle('active');
-  main.classList.toggle('blur');
-  header__logo.classList.toggle('blur');
-  header__menu.classList.toggle('blur');
-  footer.classList.toggle('blur');
-  document.body.classList.toggle('page--with-menu');
+	menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+  main.classList.toggle("blur");
+  header__logo.classList.toggle("blur");
+  header__menu.classList.toggle("blur");
+  footer.classList.toggle("blur");
+  document.body.classList.toggle("page--with-menu");
 }
 
-menuLinkAboutUs.addEventListener('click', classToggle);
-menuLinkOurProjects.addEventListener('click', classToggle);
-menuLinkOurProjects.addEventListener('click', classToggle);
-menuLinkStories.addEventListener('click', classToggle);
-menuLinkJoinTeam.addEventListener('click', classToggle);
-menuLinkContactUs.addEventListener('click', classToggle);
-menuLinkSocial.addEventListener('click', classToggle);
-menuBtn.addEventListener('click', classToggle);
-
-
-
+menuLinkAboutUs.addEventListener("click", classToggle);
+menuLinkOurProjects.addEventListener("click", classToggle);
+menuLinkOurProjects.addEventListener("click", classToggle);
+menuLinkStories.addEventListener("click", classToggle);
+menuLinkJoinTeam.addEventListener("click", classToggle);
+menuLinkContactUs.addEventListener("click", classToggle);
+menuLinkSocial.addEventListener("click", classToggle);
+menuBtn.addEventListener("click", classToggle);
 let slider = document.getElementById("range");
 let elem = document.getElementById("latestNews");
 
 slider.oninput = function() {
-    elem.style.left = - this.value * 9  + "px";
+  elem.style.left = - this.value * 9  + "px";
 }
 
+const images = document.querySelectorAll(".slider__photo img")
+const sliderPhoto = document.querySelector(".slider__photo")
+let count = 0;
+let widthSlider;
+
+let numberFoto = document.getElementById("numberFoto");
 
 
-const myTextbox = document.getElementById('subscription__form');
-myTextbox.addEventListener('keypress', checkName, false);
+function init() {
+  widthSlider = document.querySelector(".slider__photo").clientWidth;
+  sliderPhoto.style.width = widthSlider * images.length + "px";
+
+  images.forEach(img => {
+    img.style.width = widthSlider + "px";
+    img.style.height = "auto";
+  })
+
+  rollSlider();
+}
+init();
+
+
+document.querySelector(".slider__prev").addEventListener("click", function(){
+  count--;
+
+  console.log(widthSlider);
+
+  if (count < 0) {
+    count = images.length - 1;
+  }
+  rollSlider();
+
+})
+
+document.querySelector(".slider__next").addEventListener("click", function(){
+  count++;
+
+  if (count >= images.length) {
+    count = 0;
+  }
+
+  rollSlider();
+})
+
+function rollSlider() {
+  sliderPhoto.style.transform = "translateX(-" + count * widthSlider + "px)";
+  numberFoto.innerHTML = count + 1;
+}
+
+const myTextbox = document.getElementById("subscription__form");
+myTextbox.addEventListener("keypress", checkName, false);
 
 function checkName(evt) {
   const charCode = evt.charCode;
@@ -61,6 +104,4 @@ function checkName(evt) {
   }
 }
 
-
-
-document.getElementById('subscription__form').reset();
+document.getElementById("subscription__form").reset();
